55745c0b755e337910d277bb1500843846f0b55b
Fajar <fajar.baskara@gmail.com>
2010-05-12 16:59:54 +0700

    user 2 change in different place

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		// user 1 changes in same location, what happened
+		// this is user 2 change
 	}
 }
